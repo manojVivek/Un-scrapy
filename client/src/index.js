8b5c36d194d35a4e79a55fbbdfff05174f654a12
Manoj Vivek <p.manoj.vivek@gmail.com>
2017-11-26 21:29:52 +0530

    Basic working version

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,29 @@
 'use strict'
 const Fingerprint2 = require('fingerprintjs2');
-console.log("Entry");
 
 new Fingerprint2().get(function(result, components){
   console.log(result); //a hash, representing your device fingerprint
   console.log(components); // an array of FP components
-
+  var fpResult = {}
+  components.forEach(element => {
+    fpResult[element.key] = element.value;
+  });
+  console.log(JSON.stringify({fp: fpResult}))
+  fetch('/un-scrapy/result', {credentials: 'same-origin', method: 'POST', body: JSON.stringify({fp: fpResult})})
+    .then(result => {
+      if (!result.ok) {
+        console.error('Error while validating browser', err)
+        return document.write('<h1>Unexpected error, please reload the page</h1>');
+      }
+      result.json().then(data => {
+        if (data.status === 'Success') {
+          return window.location.reload(true);
+        }
+        return document.write('<h1>Access Denied</h1>');
+      })
+    })
+    .catch(err => {
+      console.error('Error while validating browser', err)
+      return document.write('<h1>Unexpected error, please reload the page</h1>');
+    });
 });
--- server/src/main.go
+++ server/src/main.go
@@ -7,15 +7,19 @@ import (
 	"io/ioutil"
 	"log"
 	"github.com/antonholmquist/jason"
+	"time"
 )
 
+const VALIDATOR_HTML = "<html><head><script src='http://127.0.0.1:8080/un-scrapy-client-bundle.js'></script></head></html>"
 func generalTrafficHandler(w http.ResponseWriter, r *http.Request) {
 	var cookie, err = r.Cookie("VALIDATED")
-	if err == nil {
-		fmt.Println("VALIDATED")
-	} else {
-		fmt.Println("NOT VALIDATED")
+	if err != nil || cookie.Value == "" {
+		fmt.Println("NOT VALIDATED" )
+		io.WriteString(w, VALIDATOR_HTML)
+		return
 	}
+	// TODO: Proxy pass to the backend and pipe the response
+	log.Println("VALIDATED")
 	io.WriteString(w, "Success"+cookie.Value)
 }
 
@@ -26,17 +30,17 @@ func unScrapyResultHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "can't read body", http.StatusBadRequest)
 		return
 	}
-
+	log.Print(string(bytes))
 	var payload, jsonErr = jason.NewObjectFromBytes(bytes)
 	if jsonErr != nil {
 		log.Printf("Error decoding JSON: %v", jsonErr)
 		http.Error(w, "Error decoding JSON", http.StatusBadRequest)
 		return
 	}
-	hasLiedLanguages, hasLiedLanguagesErr := payload.GetBoolean("has_lied_languages")
-	hasLiedResolution, hasLiedResolutionErr := payload.GetBoolean("has_lied_resolution")
-	hasLiedOs, hasLiedOsErr := payload.GetBoolean("has_lied_os")
-	hasLiedBrowser, hasLiedBrowserErr := payload.GetBoolean("has_lied_browser")
+	hasLiedLanguages, hasLiedLanguagesErr := payload.GetBoolean("fp", "has_lied_languages")
+	hasLiedResolution, hasLiedResolutionErr := payload.GetBoolean("fp", "has_lied_resolution")
+	hasLiedOs, hasLiedOsErr := payload.GetBoolean("fp", "has_lied_os")
+	hasLiedBrowser, hasLiedBrowserErr := payload.GetBoolean("fp", "has_lied_browser")
 
 	if hasLiedLanguagesErr != nil || hasLiedResolutionErr != nil || hasLiedOsErr != nil || hasLiedBrowserErr != nil {
 		log.Printf("Error while reading lied Detection Values %v %v %v %v", hasLiedLanguagesErr, hasLiedResolutionErr, hasLiedOsErr, hasLiedBrowserErr)
@@ -46,15 +50,16 @@ func unScrapyResultHandler(w http.ResponseWriter, r *http.Request) {
 
 	if hasLiedLanguages || hasLiedResolution || hasLiedOs || hasLiedBrowser {
 		log.Print("Failure")
-		io.WriteString(w, "Failure")
+		io.WriteString(w, "{\"status\" : \"Failure\"}")
 		return
 	}
+	http.SetCookie(w, &http.Cookie{Name:"VALIDATED", Value:"true", Expires: time.Now().Add(10 * time.Minute), Path: "/", MaxAge:(60*10)})
 	log.Print("Success")
-	io.WriteString(w, "Success")
+	io.WriteString(w, "{\"status\" : \"Success\"}")
 }
 
 func main() {
 	http.HandleFunc("/", generalTrafficHandler)
 	http.HandleFunc("/un-scrapy/result", unScrapyResultHandler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":9090", nil)
 }
